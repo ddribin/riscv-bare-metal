d612516e20330f5cd677e3efe18b34b13c184540
Dave Dribin <dave-vcs@dribin.org>
2023-04-03 01:13:07 -0500

    Move all .equ constants to the top

--- hello/hello.s
+++ hello/hello.s
@@ -1,8 +1,20 @@
+# UART Device, NS16550A compatible
+.equ UART_BASE, 0x10000000
+
+# Transmit Holding Register (THR)
+# Information written to this port are treated as data words
+# and will be transmitted by the UART.
+.equ UART_THR, 0
+
+# SiFive Test Device
+.equ SIFIVE_TEST, 0x100000
+.equ SIFIVE_TEST_POWEROFF_ERROR, 0x3333
+.equ SIFIVE_TEST_POWEROFF_SUCCESS, 0x5555
+.equ SIFIVE_TEST_REBOOT, 0x7777
+
+
 .section .text
 .globl _start
-
-.equ UART_BASE, 0x10000000
-.equ UART_THR,  0           # Transmit Holding Register (THR)
 
 _start:
     li      s1, UART_BASE   # s1 := 0x1000_0000
@@ -14,12 +26,8 @@ _start:
     addi    s2, s2, 1       # s2 := s2 + 1
     j       1b              # branch back to 1
 
-.equ SIFIVE_TEST, 0x100000
-.equ SIFIVE_TEST_POWEROFF_ERROR, 0x3333
-.equ SIFIVE_TEST_POWEROFF_SUCCESS, 0x5555
-.equ SIFIVE_TEST_REBOOT, 0x7777
-
 finish:
+    # Causes QEMU machine to power off, a.k.a exit
     li      s1, SIFIVE_TEST
     li      s2, SIFIVE_TEST_POWEROFF_SUCCESS
     sw      s2, 0(s1)
